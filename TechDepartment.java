570949cdfcbe36f0277c81842f9026066cefb5cc
rohan1015 <53273630+rohan1015@users.noreply.github.com>
2023-03-14 02:16:42 +0530

    Create TechDepartment.java

---
+++ TechDepartment.java
@@ -0,0 +1,22 @@
+package scheduler.department;
+
+public class TechDepartment extends SuperDepartment {
+
+	public String deparmentName() {
+		return "Tech Department";
+	}
+
+	public String getTodaysWork() {
+		return "Complete coding of module 1";
+
+	}
+
+	public String getWorkDeadline() {
+		return "Complete by EOD";
+	}
+
+	public String getTechStackInformation() {
+		return "Core Java";
+	}
+
+}
