107b5d28e4573357c814182594ec75eca41937c0
rohan1015 <53273630+rohan1015@users.noreply.github.com>
2023-03-14 02:09:52 +0530

    Create AdminDepartment.java

---
+++ AdminDepartment.java
@@ -0,0 +1,20 @@
+package scheduler.department;
+
+public class AdminDepartment extends SuperDepartment {
+
+	public String deparmentName() {
+		return " Admin Department";
+	}
+
+	public String getTodaysWork() {
+		return "Complete your documents Submission";
+
+	}
+
+	public String getWorkDeadline() {
+		return "Complete by EOD";
+		// TODO Auto-generated method stub
+
+	}
+
+}
