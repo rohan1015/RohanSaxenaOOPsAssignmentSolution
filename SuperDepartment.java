c7283c8edebfdc378c4c00e9275acb8b4805a16c
rohan1015 <53273630+rohan1015@users.noreply.github.com>
2023-03-14 02:15:02 +0530

    Create SuperDepartment.java

---
+++ SuperDepartment.java
@@ -0,0 +1,26 @@
+package scheduler.department;
+
+public class SuperDepartment {
+
+	public String deparmentName() {
+		return "Super Department";
+	}
+
+	public String getTodaysWork() {
+		return "No Work as of now";
+
+	}
+
+	public String getWorkDeadline() {
+		return "Nil";
+		// TODO Auto-generated method stub
+
+	}
+
+	public String isTodayAHoliday() {
+		return "Today is not a holiday";
+		// TODO Auto-generated method stub
+
+	}
+
+}
